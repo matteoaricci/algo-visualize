d7892df82f0d3e6735686e2f2c0acbce5bb09dd8
matteoaricci <matteo.a.ricci@gmail.com>
2020-11-15 20:56:12 -0500

    Hexes populate screen

--- src/BFS/BFS.jsx
+++ src/BFS/BFS.jsx
@@ -10,14 +10,18 @@ class BFS extends Component {
         canvasHeight: 600,
       },
       hexSize: 20,
+      hexOrigin: { x: 350, y: 300 },
     };
   }
 
   componentDidMount() {
+    this.setState({
+      hexParameters: this.getHexParameters(),
+    });
     const { canvasWidth, canvasHeight } = this.state.canvasSize;
     this.canvasHex.width = canvasWidth;
     this.canvasHex.height = canvasHeight;
-    this.drawHex(this.canvasHex, { x: 50, y: 50 });
+    this.drawHexes();
   }
 
   getHexCornerCoord(center, i) {
@@ -52,6 +56,89 @@ class BFS extends Component {
     ctx.lineTo(end.x, end.y);
     ctx.stroke();
     ctx.closePath();
+  }
+
+  drawHexes() {
+    const { canvasWidth, canvasHeight } = this.state.canvasSize;
+    const { hexWidth, hexHeight } = this.getHexParameters();
+    const hexOrigin = this.state.hexOrigin;
+    let qLeftSide = Math.round(hexOrigin.x / hexWidth) * 4;
+    let qRightSide = (Math.round(canvasWidth - hexOrigin.x) / hexWidth) * 2;
+    let rTopSide = Math.round(hexOrigin.y / (hexHeight / 2));
+    let rBottomSide = Math.round(
+      (canvasHeight - hexOrigin.y) / (hexHeight / 2)
+    );
+
+    let p = 0;
+    for (let r = 0; r <= rBottomSide; r++) {
+      if (r % 2 === 0 && r !== 0) {
+        p++;
+      }
+      for (let q = -qLeftSide; q <= qRightSide; q++) {
+        const { x, y } = this.hexToPixel(this.Hex(q - p, r));
+        if (
+          x > hexWidth / 2 &&
+          x < canvasWidth - hexWidth / 2 &&
+          y > hexHeight / 2 &&
+          y < canvasHeight - hexHeight / 2
+        ) {
+          this.drawHex(this.canvasHex, this.Point(x, y));
+          this.drawHexCoordinates(
+            this.canvasHex,
+            this.Point(x, y),
+            this.Hex(q - p, r)
+          );
+        }
+      }
+    }
+
+    let n = 0;
+    for (let r = -1; r >= -rTopSide; r--) {
+      if (r % 2 !== 0) {
+        n++;
+      }
+      for (let q = -qLeftSide; q <= qRightSide; q++) {
+        const { x, y } = this.hexToPixel(this.Hex(q + n, r));
+        if (
+          x > hexWidth / 2 &&
+          x < canvasWidth - hexWidth / 2 &&
+          y > hexHeight / 2 &&
+          y < canvasHeight - hexHeight / 2
+        ) {
+          this.drawHex(this.canvasHex, this.Point(x, y));
+          this.drawHexCoordinates(
+            this.canvasHex,
+            this.Point(x, y),
+            this.Hex(q - p, r)
+          );
+        }
+      }
+    }
+  }
+
+  Hex(q, r) {
+    return { q: q, r: r };
+  }
+
+  hexToPixel(h) {
+    let hexOrigin = this.state.hexOrigin;
+    let x = this.state.hexSize * Math.sqrt(3) * (h.q + h.r / 2) + hexOrigin.x;
+    let y = this.state.hexSize * 1.5 * h.r + hexOrigin.y;
+    return this.Point(x, y);
+  }
+
+  drawHexCoordinates(canvasID, center, h) {
+    const ctx = canvasID.getContext("2d");
+    ctx.fillText(h.q, center.x - 10, center.y);
+    ctx.fillText(h.r, center.x + 7, center.y);
+  }
+
+  getHexParameters() {
+    let hexHeight = this.state.hexSize * 2;
+    let hexWidth = (Math.sqrt(3) / 2) * hexHeight;
+    let vertDist = hexHeight * 0.75;
+    let horizDist = hexWidth;
+    return { hexWidth, hexHeight, vertDist, horizDist };
   }
 
   render() {
